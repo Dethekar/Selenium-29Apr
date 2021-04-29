844f9ea5ff9afe5a2b15ce4e19b24c6cbbef3a0c
Dethekar <dethekar@gmail.com>
2021-04-29 21:46:09 +0100

    initial commit

---
+++ src/test/java/qautomation/TestScript.java
@@ -0,0 +1,19 @@
+package qautomation;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.AfterTest;
+
+public class TestScript {
+  @Test
+  public void f() {
+  }
+  @BeforeTest
+  public void beforeTest() {
+  }
+
+  @AfterTest
+  public void afterTest() {
+  }
+
+}
